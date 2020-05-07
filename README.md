9a902859c2051bdf39f80b89a45ab026f732c6f0
liaosuyin <mail.sztaizhou.com>
2020-05-07 19:56:46 +0800

    使用mysql

---
+++ README.md
@@ -0,0 +1,11 @@
+
+npm install -g  nodemon
+npm install -g node-inspector
+
+nodemon --inspect index.js
+=========================
+1.全局安装node-inspect模块：npm install -g node-inspect
+2.通过谷歌浏览器打开：chrome://flags/#enable-devtools-experiments
+3.在cmd中输入：node --debug-brk --inspect 文件名
+4.复制cmd中Debugger listening的那个URL，在谷歌浏览器中打开之后，按F12打开谷歌开发者工具可以看到下图所示内容
+5.最终进入node-inspect调试界面，可以开始调试nodejs程序了！
--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Tabs = require('../dal/connection')
 
 var router = express.Router()
 // router.use(function (req, res, next) {
@@ -9,7 +10,10 @@ router.get('', (req, res) => {
     res.render('index', { a: 12, b: 'gg' })
 })
 router.get('/c', function (req, res) {
-    res.render('index', { a: 12, b: 'gg' })
+    debugger
+    (new Tabs()).getList(rows => {
+        res.render('index', { rows })
+    })
 })
 
 module.exports = router
