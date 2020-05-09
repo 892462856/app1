39eb965c716fa64c992f1c29ffbbae25f135d236
liaosuyin <mail.sztaizhou.com>
2020-05-09 15:40:02 +0800

    sql

--- database/app1.sql
+++ database/app1.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80000
 File Encoding         : 65001
 
-Date: 2020-05-07 18:39:11
+Date: 2020-05-09 15:39:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,46 +20,69 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `articles`;
 CREATE TABLE `articles` (
-  `id` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `content` text CHARACTER SET utf8 NOT NULL,
   `datetime` datetime NOT NULL,
-  `link` varchar(1000) DEFAULT NULL,
-  `fetchCode` varchar(10) DEFAULT NULL,
+  `order` int(4) NOT NULL DEFAULT '1',
+  `link` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
+  `fetchCode` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of articles
+-- Table structure for articlesclassify
 -- ----------------------------
+DROP TABLE IF EXISTS `articlesclassify`;
+CREATE TABLE `articlesclassify` (
+  `articles_id` varchar(255) NOT NULL,
+  `classifys_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`articles_id`,`classifys_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for articlestabs
+-- ----------------------------
+DROP TABLE IF EXISTS `articlestabs`;
+CREATE TABLE `articlestabs` (
+  `articles_id` varchar(255) NOT NULL,
+  `tabs_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`articles_id`,`tabs_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for classifys
 -- ----------------------------
 DROP TABLE IF EXISTS `classifys`;
 CREATE TABLE `classifys` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `order` int(11) NOT NULL DEFAULT '1',
-  `enabled` bit(1) NOT NULL DEFAULT b'1',
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of classifys
+-- Table structure for menu
 -- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` varchar(255) NOT NULL,
+  `parentId` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `order` int(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tabs
 -- ----------------------------
 DROP TABLE IF EXISTS `tabs`;
 CREATE TABLE `tabs` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `enabled` bit(1) NOT NULL DEFAULT b'1',
+  `id` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of tabs
--- ----------------------------
